7a99f1c4826b954a161c582615344a5d0af4b2f7
Benny Powers <web@bennypowers.com>
2019-12-20 12:43:04 +0200

    docs: update demo

---
+++ .storybook/preview.js
@@ -0,0 +1,31 @@
+import {
+  addParameters,
+  addDecorator,
+  setCustomElements,
+  withA11y
+} from '@open-wc/demoing-storybook';
+
+const handleAsJson = response => response.json()
+
+async function run() {
+  await fetch(new URL('../custom-elements.json', import.meta.url))
+    .then(handleAsJson)
+    .then(setCustomElements)
+
+  addDecorator(withA11y);
+
+  addParameters({
+    a11y: {
+      config: {},
+      options: {
+        checks: { 'color-contrast': { options: { noScroll: true } } },
+        restoreScroll: true
+      }
+    },
+    docs: {
+      iframeHeight: '200px'
+    }
+  });
+}
+
+run();
