ac91a2f740be091d78be666b6c786807ef70ce1d
Benny Powers <web@bennypowers.com>
2019-12-20 12:42:46 +0200

    test: update tests

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,13 @@ const merge = require('deepmerge');
 module.exports = config => {
   config.set(
     merge(createDefaultConfig(config), {
-      files: config.grep ? [config.grep] : ['src/*.test.js'],
+      files: [
+        { pattern: config.grep ? config.grep : 'src/**/*.test.js', type: 'module' },
+      ],
       esm: {
         nodeResolve: true,
         babel: true,
-      }
+      },
     })
   );
 
