6b11343f8ec19cfdf91dc506afa2274fc8cd35b3
Benny Powers <web@bennypowers.com>
2020-01-19 09:54:40 +0200

    test: back to 100% coverage

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,21 @@ const merge = require('deepmerge');
 module.exports = config => {
   config.set(merge(createDefaultConfig(config), {
     ...config.autoWatch ? { mochaReporter: { output: 'autowatch' } } : {},
-    files: [{ pattern: config.grep ? config.grep : 'src/**/*.test.js', type: 'module' }],
+    files: [
+      { pattern: config.grep ? config.grep : 'src/**/*.test.js', type: 'module' },
+      { pattern: config.grep ? config.grep : 'test/*.test.js', type: 'module' },
+    ],
     esm: {
       nodeResolve: true,
       babel: true,
-      coverageExclude: ['src/**/*.test.js'],
+      coverageExclude: [
+        'src/*.test.js',
+        'src/**/*.test.js',
+        'node_modules',
+        'stories/*',
+        'test/*',
+        'src/lib/strings.test.js',
+      ],
     },
   }));
   return config;
--- src/lib/strings.js
+++ src/lib/strings.js
@@ -4,31 +4,17 @@ import { memoize } from '@pacote/memoize';
 
 const identity = x => x;
 
-/**
- * camelCase a string
- * @type {(string: string) => string}
- */
+/** camelCase a string */
 export const camel = memoize(identity, eagerCamel);
 
-/**
- * dash-case a string
- * @type {(string: string) => string}
- */
+/** dash-case a string */
 export const dash = memoize(identity, eagerDash);
 
-function randomReplacer(c) {
-  return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
-}
-
-function getRandomString() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, randomReplacer);
-}
-
 /**
- * Generates a random mount point (UUID v4) for Stripe Elements. This will allow multiple
+ * Generates a random mount point for Stripe Elements. This will allow multiple
  * Elements forms to be embedded on a single page.
  * @return {String} mount element id
  */
 export function generateRandomMountElementId() {
-  return `stripe-elements-mount-point-${getRandomString()}`;
+  return `stripe-elements-mount-point-${(Date.now() + (Math.random() * 1000)).toString(36).substr(0, 8)}`;
 }
