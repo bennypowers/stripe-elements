7d6e49ee11b18e71a6260e712ec07607a0aefc20
Benny Powers <web@bennypowers.com>
2020-01-27 12:09:18 +0200

    chore: fix ci

--- scripts/finish-declarations.sh
+++ scripts/finish-declarations.sh
@@ -1,14 +1,11 @@
 echo "declare global { interface HTMLElementTagNameMap { 'stripe-elements': StripeElements; } }" >> stripe-elements.d.ts
 echo "declare global { interface HTMLElementTagNameMap { 'stripe-payment-request': StripePaymentRequest; } }" >> stripe-payment-request.d.ts
-echo $(cat <<- END
-import { StripeElements } from './stripe-elements';
-import { StripePaymentRequest } from './stripe-payment-request';
-declare global {
-  interface HTMLElementTagNameMap {
-    'stripe-elements': StripeElements;
-    'stripe-payment-request': StripePaymentRequest;
-  }
-}
-END) >> index.d.ts
-
+echo "import { StripeElements } from './stripe-elements';" >> index.d.ts
+echo "import { StripePaymentRequest } from './stripe-payment-request';" >> index.d.ts
+echo "declare global {" >> index.d.ts
+echo "  interface HTMLElementTagNameMap {" >> index.d.ts
+echo "    'stripe-elements': StripeElements;" >> index.d.ts
+echo "    'stripe-payment-request': StripePaymentRequest;" >> index.d.ts
+echo "  }" >> index.d.ts
+echo "}" >> index.d.ts
 node scripts/privatize.js
