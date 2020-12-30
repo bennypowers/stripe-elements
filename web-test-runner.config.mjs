f368db44bb13927d2c5f1052a1518a16c5bd7bf0
Benny Powers <web@bennypowers.com>
2020-12-30 22:20:16 +0200

    chore: karma -> @web/test-runner

---
+++ web-test-runner.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { fromRollup } from '@web/dev-server-rollup';
+
+import _litcss from 'rollup-plugin-lit-css';
+
+const litcss = fromRollup(_litcss);
+
+/** @type {import('@web/test-runner').TestRunnerConfig} */
+const config = {
+  nodeResolve: true,
+
+  files: [
+    'test/**/*.test.ts',
+  ],
+
+  mimeTypes: {
+    '**/*.json': 'js',
+    '**/src/*.css': 'js',
+  },
+
+  coverageConfig: {
+    thresholds: {
+      global: {
+        statements: 100,
+        lines: 100,
+        branches: 100,
+        functions: 100,
+      },
+    },
+  },
+
+  plugins: [
+    litcss(),
+    esbuildPlugin({ ts: true }),
+  ],
+};
+
+export default config;
